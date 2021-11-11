f6fd08cbd8645830703e8f851e66f798d9201db3
Nicolas Brouwers <nicolas.b.95@hotmail.com>
2021-11-11 16:04:54 +0100

    chall 3 rails

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,32 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new # needed to instantiate the form_for
+  end
+
+ def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    # no need for app/views/lists/destroy.html.erb
+    redirect_to lists_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
